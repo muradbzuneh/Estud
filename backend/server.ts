2fafdde9da72c0353059d8b1151a62dbfe1c6a7f
Mbeth <muradbzuneh@gmail.com>
2026-02-17 03:18:57 -0800

    connect to mongodb and format the structure of the file

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,11 @@
-import app  = require('./app') ;
+import app  = require('./src/app') ;
 import dotenv = require('dotenv');
+import connectDB = require('./src/config/db');
+
+
 
 dotenv.config();
-
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
---
+++ backend/src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose = require("mongoose");
+
+const connectDB = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGO_URL as string)
+        console.log("mongodb connected")
+    }
+    catch (error: any) {
+        
+ console.error("Database connection failed", error.message);
+ process.exit(1)
+    }
+}
+
+export = connectDB
